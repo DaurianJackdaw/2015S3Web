cb686544bbb3c41682216df1369def6c5b444fa5
DaurianJackdaw <junjiechen96@gmail.com>
2015-04-09 23:14:36 +0800

    hw3 final(? without multi-thread) version

--- hw3/word_analysis.rb
+++ hw3/word_analysis.rb
@@ -2,27 +2,63 @@ require 'nokogiri'
 require 'net/http'
 #require 'open-uri'
 
-#find target url
-redirect_url = 'http://en.wikipedia.org/wiki/Special:Random'
-url = Net::HTTP.get_response(URI.parse(redirect_url))['location']
-p url
+#redirects url once
+#if the url multi-redirects, use this function until returns nil
+#and don't forget to check for infinite redirection
+def url_redirect (url)
+	return Net::HTTP.get_response(URI.parse(url))['location']
+end
 
-#split url into 2 parts for HTTP.get()
-host = url[/(?<=http:\/\/)[^\/]+/]
-p host
-path = url.sub('http://', "").sub(host, "")
-p path
+#open url with Nokogiri
+def url_open (url)
+	#split url into 2 parts for Net:HTTP below
+	host = url[/(?<=http:\/\/)[^\/]+/]
+	path = url.sub('http://', "").sub(host, "")
 
-#download target webpage
-doc = Net::HTTP.get(host, path)
-contents = Nokogiri::HTML(doc)
+	return Nokogiri::HTML(Net::HTTP.get(host, path))
+end
 
 #process webpage content
-contents.css('script, style').each {|node| node.remove}
-words = contents.text.split("\s")
-words.each do |word|
-	word.sub!(/^[(\"]/, "")
-	word.sub!(/\W+$/, "")
+def webpage_formatting (contents)
+	#remove tags like <script> because they mess up with Nokogiri
+	#add other tags to this variable if unwanted stuff show up in Nokogiri::HTML.text
+	to_be_removed = 'script, style'
+	contents.css(to_be_removed).each {|node| node.remove}
+
+	words = contents.text.squeeze("\s").split("\s")
+	#remove punctuations in front and/or end of words
+	words.each do |word|
+		word.sub!(/^[\[(\"]/, "")
+		word.sub!(/\W+$/, "")
+		#uncomment the following line if you want this program to be case insensitive
+		#word.downcase
+	end
+	#remove strings with pure punctuation
+	words.delete_if {|empty| empty == ""}
+
+	return words
 end
-words.delete_if {|empty| empty == ""}
-p words
+
+#analysis of word frequency
+def word_frequency (words)
+	h = Hash.new(0)
+
+	words.each do |word|
+		h[word] += 1
+	end
+
+	return h.to_a.sort{|a, b| a[1] <=> b[1]}
+end
+
+#main
+link = 'http://en.wikipedia.org/wiki/Special:Random'
+page_count = 10
+n_of_most_frequent = 100
+webpage_contents = []
+
+for count in 1..page_count
+	target_url = url_redirect(link)
+	webpage_contents.concat(webpage_formatting(url_open(target_url)))
+end
+
+p word_frequency(webpage_contents).reverse.slice(0, n_of_most_frequent).to_h
